2bea668ca261f2eb106ce379cb1309b38bcec87b
Gean Fernandes da Silva <gean.fgsilva@gmail.com>
2023-09-14 23:43:09 -0300

    update

--- app.py
+++ app.py
@@ -9,6 +9,32 @@ from sklearn.pipeline import Pipeline  # Importe o Pipeline aqui
 
 app = Flask(__name__)
 
+list_pendentes = [
+    {"Nome": "Tiago Silva", "N° Contrato": 61, "Valor": "R$320.800", "Data": "25/04/2011"},
+    {"Nome": "Luciana Oliveira", "N° Contrato": 63, "Valor": "R$170.750", "Data": "25/07/2011"},
+    {"Nome": "Felipe Santos", "N° Contrato": 66, "Valor": "R$86.000", "Data": "12/01/2009"},
+    {"Nome": "Carla Souza", "N° Contrato": 22, "Valor": "R$433.060", "Data": "29/03/2012"},
+    {"Nome": "Marcos Costa", "N° Contrato": 33, "Valor": "R$162.700", "Data": "28/11/2008"},
+    {"Nome": "Renata Lima", "N° Contrato": 61, "Valor": "R$372.000", "Data": "02/12/2012"},
+    {"Nome": "Eduardo Pereira", "N° Contrato": 59, "Valor": "R$137.500", "Data": "06/08/2012"},
+    {"Nome": "Mariana Fernandes", "N° Contrato": 55, "Valor": "R$327.900", "Data": "14/10/2010"},
+    {"Nome": "Fernando Alves", "N° Contrato": 39, "Valor": "R$205.500", "Data": "15/09/2009"},
+    {"Nome": "Sabrina Barbosa", "N° Contrato": 23, "Valor": "R$103.600", "Data": "13/12/2008"},
+    {"Nome": "Ricardo Oliveira", "N° Contrato": 30, "Valor": "R$90.560", "Data": "19/12/2008"},
+    {"Nome": "Larissa Costa", "N° Contrato": 22, "Valor": "R$342.000", "Data": "03/03/2013"},
+    {"Nome": "Isabela Souza", "N° Contrato": 36, "Valor": "R$470.600", "Data": "16/10/2008"},
+    {"Nome": "Roberto Santos", "N° Contrato": 43, "Valor": "R$313.500", "Data": "18/12/2012"},
+    {"Nome": "Luisa Fernandes", "N° Contrato": 19, "Valor": "R$385.750", "Data": "17/03/2010"},
+    {"Nome": "Pedro Lima", "N° Contrato": 66, "Valor": "R$198.500", "Data": "27/11/2012"},
+    {"Nome": "Ana Rodrigues", "N° Contrato": 64, "Valor": "R$725.000", "Data": "09/06/2010"},
+    {"Nome": "Fernando Silva", "N° Contrato": 59, "Valor": "R$237.500", "Data": "10/04/2009"}
+]
+
+@app.route('/tabela', methods=['GET'])
+def tabela():
+    return render_template('table.html', data=list_pendentes)
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
 
@@ -91,17 +117,22 @@ def predict():
 
 import random
 
-@app.route('/random_data', methods=['GET'])
-def random_data():
-    try:
-        # Gerar dados aleatórios para 10 pessoas
-        adimplente = random.randint(0, 10)
-        inadimplente = 10 - adimplente
+@app.route('/teste', methods=['POST'])
+def teste():
+    data = request.json  # Obtenha os dados da requisição POST
 
-        return jsonify({'Adimplente': adimplente, 'Inadimplente': inadimplente})
-    except ValueError as e:
-        # Em caso de erro, retorne uma mensagem de erro
-        return jsonify({'error': str(e)})
+    print(data)
+
+    nome = data.get('nome', '')
+    print(nome)
+    # Faça qualquer processamento necessário com os dados aqui
+    # ...
+
+    # Suponha que o processamento tenha sido bem-sucedido
+    resultado = {"status": "sucesso", "mensagem": "Processamento bem-sucedido"}
+
+    # Retorne uma resposta em formato JSON com o status
+    return jsonify(resultado)
 
 if __name__ == '__main__':
     app.run(debug=True)
